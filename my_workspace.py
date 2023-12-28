3be9fff2233525d34dd1ee1705f9096a2648db8b
Miklushov N. <miklfirm@bk.ru>
2023-12-28 10:16:57 +0300

    Less12 it's done

--- my_workspace.py
+++ my_workspace.py
@@ -48,6 +48,14 @@ class Users_dict:
         with open(r'users.yaml', 'r') as file:
             pprint(yaml.load(file, Loader=yaml.FullLoader))
 
+    def setUsersInFileCSV(self):
+        df = pandas.DataFrame(self.users)
+        df.to_csv("users.csv",index=False)
+
+    def getUsersInFileCSV(self):
+        df = pandas.read_csv("users.csv")
+        print(df)
+
     def setUsersInFileJSON(self, filename):
         with open(filename, 'w') as file:
           file.write(json.dumps(self.users, indent=4))
@@ -58,6 +66,18 @@ class Users_dict:
             # json.dumps(self.users, indent=4)
     def getUsers(self):
         pprint(self.users)
+
+    def YamlToJson(self):
+        with open("users.yaml", 'r') as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+            with open("users_yaml_to_json.json", 'w') as file2:
+                file2.write(json.dumps(data, indent=4))
+
+    def JsonToYaml(self):
+        with open("users.json", 'r') as file:
+           data = json.load(file)
+           with open("users_json_to_yaml.yaml", 'w') as file2:
+                yaml.dump(data, file2)
 
 # ---------------------------------------------------
 my_users = [
@@ -88,9 +108,12 @@ dict_users2 = Users_dict(my_users)
 # dict_users2.getUsers()
 # dict_users2.setUsersInFileYaml()
 # dict_users2.getUsersInFileYaml()
-dict_users2.setUsersInFileJSON(filename)
-dict_users2.getUsersInFileJSON(filename)
-
+# dict_users2.setUsersInFileJSON(filename)
+# dict_users2.getUsersInFileJSON(filename)
+# dict_users2.setUsersInFileCSV()
+# dict_users2.getUsersInFileCSV()
+dict_users2.YamlToJson()
+dict_users2.JsonToYaml()
 print("-------list----------")
 list_users_old = [User(1, "miklushov","mikl",34, 146.5),
                   User(2, "ivanov","boba",30, 0.0),
@@ -98,5 +121,5 @@ list_users_old = [User(1, "miklushov","mikl",34, 146.5),
 
 all_users = Users(list_users_old)
 
-all_users.getUsers()
+# all_users.getUsers()
 
